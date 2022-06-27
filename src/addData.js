7b23d6294b5fe1d29e9aebb988789074627ffff6
xavigrausebastian <xavier.grau@kreatings.com>
2022-06-27 17:51:35 +0200

    CREATE addData file to add data to the database

---
+++ src/addData.js
@@ -0,0 +1,23 @@
+const data = require('./public/uploads/finals/barcelona/json/barcelona.json');
+const axios = require("axios");
+
+
+init().then(() => {
+	console.log("ya estoy :)")
+});
+
+async function init() {
+
+	for (let i = 0; i < data.length; i++) {
+
+		await axios.post('http://localhost:4000/v1/add/', data[i])
+			.then((response) => {
+				console.log(response.data.message);
+			})
+			.catch((err) => {
+				console.log(err.response?.data);
+			})
+	}
+
+}
+
